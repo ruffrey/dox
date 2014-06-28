91e73655b027fb577d15a4cda4fc422334348056
Jeff Parrish <jparrish@digium.com>
2014-06-28 09:30:44 -0700

    now jsdoxy parses multiple files or folders

--- tasks/jsdoxy.js
+++ tasks/jsdoxy.js
@@ -10,7 +10,8 @@ var exec = require('child_process').exec,
     fs = require('fs'),
     path = require('path'),
     rimraf = require('rimraf'),
-    jade = require('jade');
+    jade = require('jade'),
+    async = require('async');
 
 
 module.exports = function(grunt) {
@@ -23,16 +24,12 @@ module.exports = function(grunt) {
         doxPath = path.resolve(__dirname,'../'),
         _opts = this.options(),
         _args = [],
-        outputFile = path.resolve(dest, "output.json");
+        outputFile = "jsdoxy-output.json";
 
-    if(!fs.existsSync(dest)) {
-      grunt.log.write('making ' + dest);
-      fs.mkdirSync(dest);
-    }
-    if(!fs.existsSync(outputFile)) {
-      grunt.log.write('making ' + outputFile);
-      fs.writeFileSync(outputFile, "");
-    }
+    // if(!fs.existsSync(outputFile)) {
+    //   grunt.log.writeln('jsdoxy: making file ' + outputFile);
+    //   grunt.file.write(outputFile, " ");
+    // }
 
     // Absolute path to jsdoxy
     var jsdoxy = [doxPath, 'bin', 'jsdoxy'].join(path.sep);
@@ -40,40 +37,58 @@ module.exports = function(grunt) {
     // Cleanup any existing docs
     rimraf.sync(dest);
 
-    // _args.push('<');
-    // _args.push(dir);
-    // _args.push('>');
-    // _args.push(dest);
+    var executeFiles = [];
+    var output = [];
 
-    // Set options to arguments
-    // if(_opts.title){
-    //   // _args.push('--title');
-    //   _args.push('"' + _opts.title + '"');
-    // }
+    dir.forEach(function(file) {
+      executeFiles.push(function(cb){
 
-    if (!_opts.template) {
-      grunt.log.error("jsdoxy ERROR: missing jade `template` option");
-      return;
-      // _args.push('--template');
-      // _args.push('"' + _opts.template + '"');
-    }
-
-    // Pass through ignore params if set
-    // if (this.data.ignore) {
-    //   _args.push('--ignore');
-    //   this.data.ignore.forEach(function(ignorePath) {
-    //     _args.push(doxPath + ignorePath);
-    //   });
-      
-    // }
-
-    exec(jsdoxy + ' < ' + dir.join(' ') + " > " + outputFile, {maxBuffer: 5000*1024}, function(error, stout, sterr){
-      if (error) { grunt.log.error("jsdoxy ERROR:  "+ error + "\n" + error.stack); }
-      if (!error) {
-        grunt.log.ok('Directory "' + dir + '" doxxed by jsdoxy.');
-        done();
-      }
+        var filepath = path.join(dest, file + ".json");
+        grunt.file.write(filepath, " ");
+        exec(
+          jsdoxy + ' < ' + file + " > " + filepath, 
+          {maxBuffer: 5000*1024}, 
+          function(error, stout, sterr) {
+            if (error) { 
+              grunt.log.error("jsdoxy ERROR:  "+ error + "\n" + error.stack);
+              cb(err); 
+            }
+            if (!error) {
+              grunt.log.ok( file + '" got doxxed, son.');
+              output = output.concat( grunt.file.readJSON(filepath) );
+              cb();
+            }
+        });
+      });
     });
+
+    async.series(executeFiles, function(err) {
+      if(err) return;
+
+      if(!_opts.template) return done();
+          
+        grunt.log.writeln('Jadifying the output using template ' + _opts.template);
+
+        var organizedByClass = {};
+
+        output.forEach(function(comment) {
+          comment.tags.forEach(function(tag) {
+            if(tag.type == "class")
+            {
+              organizedByClass[tag.string] = comment;
+              return false;
+            }
+          });
+        });
+
+        grunt.file.write('obj-'+outputFile, JSON.stringify(organizedByClass, null, 4));
+
+        // var html = jade.renderFile('filename.jade', merge(options, locals));
+
+        done();
+    });
+      
+
   });
 
 };
